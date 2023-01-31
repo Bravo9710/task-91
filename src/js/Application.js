2fb396cb9aeb3ae53962dfac73d94b68d3529401
vvenkov <vvenkov@htmlbox.net>
2023-01-31 22:58:40 +0200

    Done

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,16 +10,26 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+    this._beat = new Beat()
 
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+    this._beat.on(Beat.events.BIT, () => {
+      if (count < lyrics.length) {
+        this._create(lyrics, count);
+        count++;
+      }
+    })
 
-    document.querySelector(".main").appendChild(message);
 
     this.emit(Application.events.READY);
+  }
+
+  _create(lyricsArray, index) {
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = lyricsArray[index];
+    document.querySelector(".main").appendChild(message);
   }
 }
